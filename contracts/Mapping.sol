e51701f000f6d76ebb4b20c69737120d5f5e8806
mayurbagga <mayurbagga@hotmail.com>
2024-03-12 14:58:20 +0530

    push from laptop

---
+++ contracts/Mapping.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract Mapping {
+    mapping(uint => address) public idToAddress; 
+    mapping(address => address) public swapPair; 
+    
+   
+    //    uint256 id;
+    //    uint256 score; 
+    //}
+    // mapping(Struct => uint) public testVar;
+
+    function writeMap (uint _Key, address _Value) public{
+        idToAddress[_Key] = _Value;
+    }
+}
+
